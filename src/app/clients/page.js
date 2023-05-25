8eb89b6b282078e539740b46899de8369a882304
ddS08 <80191264+ddS08@users.noreply.github.com>
2023-05-25 15:25:25 +0530

    15:25 (25/05)

--- src/app/clients/page.js
+++ src/app/clients/page.js
@@ -1,7 +1,22 @@
-
+import Link from 'next/link';
 function ClientsPage(){
+    const clients = [
+        {id:'DS',name:'Dhyaan'},
+    {id:'max',name:'Maximilian'},
+];
     return <div>
         <h1>The CLients Page</h1>
+        <ul>
+            {clients.map((client)=> 
+            (<li key={client.id}>
+                <Link href={{
+                    pathname:'/clients/[id]',
+                    query:{id:client.id},
+
+                }}>{client.name}</Link>
+            </li>
+            ))}
+        </ul>
     </div>
 }
 export default ClientsPage;
