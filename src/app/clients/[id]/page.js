700816c93baa2833515568994a643924daef24ba
ddS08 <80191264+ddS08@users.noreply.github.com>
2023-05-25 16:31:07 +0530

    16:30 (25/05)

--- src/app/clients/[id]/page.js
+++ src/app/clients/[id]/page.js
@@ -1,13 +1,21 @@
 'use client'
-import { useParams,usePathname } from "next/navigation";
+import { useRouter,useParams,usePathname } from "next/navigation";
 function ClientProjectsPage(){
     const params=useParams();
     const path=usePathname();
+    const router = useRouter();
     console.log(params);
     console.log(path);
+    console.log(router);
+    function loadProjectHandler(){
+        //load data...
+        router.push('/clients/DS/ProjectA');
+
+    }
     return(
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
